efdc4e91fa78e19b3047277447563447f6af2e36
MKS-Student <mks.student@hackreactor.com>
2017-04-03 13:59:25 -0700

    POST new requests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,40 @@
-// YOUR CODE HERE:
+class App {
+  constructor() {
+    
+  }
+
+  init () {
+
+  }
+
+  send (message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  }
+
+  fetch () {
+    
+  }
+}
+
+var app = new App();
+
+var message = {
+  username: 'test',
+  text: 'this is a test',
+  roomname: 'lobby'
+};
+
+app.send(message);
