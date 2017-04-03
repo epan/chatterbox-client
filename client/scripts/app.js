26509db94d36e21bb54686a73a8eaa48721e22a9
MKS-Student <mks.student@hackreactor.com>
2017-04-03 15:49:41 -0700

    fetch, render, and clear messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,7 @@
-class App {
-  constructor() {
-    
+class Chatterbox {
+  constructor(value) {
+    this.name = value;
+    this.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
   }
 
   init () {
@@ -10,12 +11,12 @@ class App {
   send (message) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+      url: this.server,
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent');
+        console.log();
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -24,17 +25,36 @@ class App {
     });
   }
 
-  fetch () {
-    
+  fetch (successCallback) {
+    return $.ajax({
+      url: this.server,
+      type: 'GET',
+      contentType: 'application/json',
+      success: function(data) {
+        successCallback(data);
+      },
+      error: function () {
+        console.log('it broke');
+      }
+    });
+  }
+
+  renderAllMessages () {
+    var thisObj = this;
+    this.fetch(function(data) {
+      data.results.forEach(function(message) {
+        thisObj.renderMessage(message);
+      });
+    });
+  }
+
+  clearMessages () {
+    $('#chats').children().remove();
+  }
+
+  renderMessage (message) {
+    $('#chats').append(`<p>[${message.roomname}] @${message.username}: ${message.text}</p>`);
   }
 }
 
-var app = new App();
-
-var message = {
-  username: 'test',
-  text: 'this is a test',
-  roomname: 'lobby'
-};
-
-app.send(message);
+var app = new Chatterbox('alex');
